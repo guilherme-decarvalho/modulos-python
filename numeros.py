4b7f985f5b0ba55f1f0fcf97ccd4ed54c4b6c7b8
Guilherme de Carvalho <guilhermedecarvalho.dev@gmail.com>
2024-05-27 11:10:58 -0300

    Módulos

---
+++ numeros.py
@@ -0,0 +1,9 @@
+from uteis import numeros
+
+num = int(input('Digite um valor: '))
+fat = numeros.fatorial(num)
+# fat = fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {numeros.dobro(num)}')
+# print(f'O dobro de {num} é {dobro(num)}')
+
